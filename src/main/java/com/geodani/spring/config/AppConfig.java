e7f7473576dab1965232ccb47ff4e9a118eaff33
Catalina <cristea.catalina89@gmail.com>
2019-06-22 14:14:50 +0300

    Initial

---
+++ src/main/java/com/geodani/spring/config/AppConfig.java
@@ -0,0 +1,10 @@
+package com.geodani.spring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"com.geodani.spring"})
+public class AppConfig {
+
+}
